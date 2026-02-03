cc246f03e2bc31fa1b9eb23bd996891c4a8e7f13
MonsieurMeh <mensoirmeh@gmail.com>
2026-02-02 18:00:05 -0800

    fix: turn Vector3 Position into propery referencing NewtonsoftJSON-friendly floats internally

--- SpawnRegionInfo.cs
+++ SpawnRegionInfo.cs
@@ -7,8 +7,24 @@ namespace StolenMeatMod
     class SpawnRegionInfo
     {
         public string Scene;
+        public float PositionX;
+        public float PositionY;
+        public float PositionZ;
         public string ObjectGuid;
-        public Vector3 Position;
         public float DespawnTime;
+
+        public Vector3 Position
+        {
+            get
+            {
+                return new Vector3(PositionX, PositionY, PositionZ);
+            }
+            set
+            {
+                PositionX = value.x;
+                PositionY = value.y;
+                PositionZ = value.z;
+            }
+        }
     }
 }
